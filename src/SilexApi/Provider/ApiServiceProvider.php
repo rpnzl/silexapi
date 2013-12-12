582d34d62a14ec64987b39f4b56cbdd832e6d262
Michael Giuliana <mike@rpnzl.com>
2013-12-12 10:58:17 -0500

    Updated structure

---
+++ src/SilexApi/Provider/ApiServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+namespace SilexApi\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class ApiServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app['api'] = $app->share(function () use ($app) {
+            return new ApiControllerProvider;
+        });
+
+        return $app['api'];
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}

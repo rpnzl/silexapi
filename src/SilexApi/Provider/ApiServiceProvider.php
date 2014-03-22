74101950b37a180fb8707377653f4d41bca4cb25
Michael Giuliana <mike@rpnzl.com>
2014-03-21 23:59:55 -0400

    added api.routes property to app

--- src/SilexApi/Provider/ApiServiceProvider.php
+++ src/SilexApi/Provider/ApiServiceProvider.php
@@ -15,10 +15,43 @@ class ApiServiceProvider implements ServiceProviderInterface
             // 803 => 404
         );
 
+        // 
         $app['api'] = $app->share(function () use ($app) {
             return new ApiControllerProvider;
         });
 
+        // 
+        $app['api.routes'] = $app->share(function () use ($app) {
+            if (!is_dir($app['api.source_path'])) {
+                throw new RuntimeException("api.source_path must be defined to use api.routes!");
+            }
+
+            $classes = array_filter(array_map(function ($v) use ($app) {
+                preg_match('#^'.$app['api.namespace'].'#', $v, $matches);
+                return count($matches) ? $v : null;
+            }, array_keys(Composer\Autoload\ClassMapGenerator::createMap($app['api.source_path']))));
+
+            $routes = array();
+            foreach ($classes as $v) {
+                $class = new ReflectionClass($v);
+                $class_routes = array_filter(array_map(function ($v) use ($class) {
+                    $http_methods = array('get', 'post', 'put', 'delete');
+                    preg_match('#^'.implode('|', $http_methods).'#', $v->name, $matches);
+                    if (in_array($v->name, $http_methods)) {
+                        return strtoupper($matches[0]).' /'.strtolower(str_replace('\\', '/', $class->name));
+                    } else if (count($matches)) {
+                        $segment = str_replace($matches[0], '', $v->name);
+                        $base_route = str_replace('\\', '/', $class->name);
+                        return strtoupper($matches[0]).' /'.strtolower($base_route.'/'.$segment);
+                    }
+                }, $class->getMethods(ReflectionMethod::IS_PUBLIC)));
+                $routes = array_merge($routes, $class_routes);
+            }
+
+            return $routes ?: array();
+        });
+
+        // 
         $app->before(function (Request $request) use ($app) {
             if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
                 $data = json_decode($request->getContent(), true);
